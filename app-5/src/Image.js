55332921fc95044d0a3c8d9d13a76b65ee9f4696
Rob-Swensen <rg.swensen801@gmail.com>
2020-02-26 13:54:44 -0700

    Second Commit

---
+++ app-5/src/Image.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+
+class Image extends Component{
+    constructor(){
+        super();
+
+    }
+    render(){
+        return(
+            <div>
+                <img src={this.props.url}/>
+                <div>Error 409</div>
+            </div>
+        )
+    }
+}
+
+export default Image;
