d78c7e6ee644239c7e10b1d056c60697e2a9e168
Rob-Swensen <rg.swensen801@gmail.com>
2020-02-26 15:53:25 -0700

    Third Commit

---
+++ app-7/src/Components/NewTask.js
@@ -0,0 +1,31 @@
+import React, {Component} from 'react';
+
+class NewTask extends Component{
+    constructor(){
+        super();
+        this.state = {
+            input: ''
+        }
+        this.handleAdd = this.handleAdd.bind(this)
+    }
+
+handleInput(val){
+    this.setState({input: val})
+}
+
+handleAdd(){
+    this.props.add(this.state.input);
+    this.setState({input: ''})
+}
+
+render(){
+    return(
+        <div>
+            <input value={this.state.input} placeholder='Enter new task' onChange={(e) => this.handleInput(e.target.value)}/>
+            <button onClick={this.handleAdd}>Add</button>
+        </div>
+    )
+}
+}
+
+export default NewTask;
